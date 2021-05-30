6cb1f977b51f00b2012e01c1ac922553b9c09b4a
MicahPB01 <micahpuccioball@gmail.com>
2021-05-30 13:01:43 -0400

    Initial commit

---
+++ src/main/java/oop/example/App.java
@@ -0,0 +1,32 @@
+package oop.example;
+
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+        int width;
+        int length;
+        int area;
+        int feetPerGallon = 350;
+        int numGallons;
+
+        System.out.printf("Length: ");
+        length = Integer.parseInt(input.nextLine());
+        System.out.printf("Width: ");
+        width = Integer.parseInt(input.nextLine());
+
+        area = width*length;
+
+        numGallons = area / feetPerGallon;
+        if(numGallons % area != 0)   {
+            numGallons = numGallons + 1;
+        }
+
+        System.out.printf("You will need to purchase %d gallons of paint to cover %d square feet.\n", numGallons, area);
+
+
+    }
+}
